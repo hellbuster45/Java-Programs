7b2a97cc20387ea158364c93dda98ce6cde1e279
hellbuster45 <rupenkumar22@gmail.com>
2022-08-22 21:00:01 +0530

    Program to convert decimal into binary number...

---
+++ Decimal_to_Binary.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Decimal_to_Binary
+{
+    public static void main(String[] args)
+    {
+        int dec, quot, rem;       
+        int i = 0, j = 0;
+        int arr[] = new int[20];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a decimal number: ");
+        dec = scan.nextInt();
+        do
+        {
+            quot =  (dec / 2);
+            rem = (dec % 2);
+            dec = quot;
+            arr[i] = rem;
+            i++;
+        }while(quot != 0);
+        System.out.print("The binary conversion is: ");
+        for(j = i - 1; j>=0; j--)
+        {
+            System.out.print(arr[j]);
+        }
+        scan.close();
+    }
+}
